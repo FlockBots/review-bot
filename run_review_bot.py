237d2199bfd82e2f77dfa5adf1b2722b6ff282fc
Stan Janssen <stanjanssen@outlook.com>
2014-12-27 00:46:43 +0000

    really completed merge

--- run_review_bot.py
+++ run_review_bot.py
@@ -155,12 +155,7 @@ class ReviewBot(Bot):
 
         list_pattern = self.triggers['list']
         list_matches = list_pattern.findall(body)
-<<<<<<< HEAD
-        inventory_pattern = self.triggers['inventory']
-        inventory_matches = list_pattern.search(body)
-=======
 
->>>>>>> a525b56b437ef934960a3b2ff8da5048341c104a
         add_pattern = self.triggers['add']
         add_matches = add_pattern.search(body)
 
@@ -169,13 +164,7 @@ class ReviewBot(Bot):
         if add_matches:
             reply += addfn(post)
             reply += '\n\n'
-<<<<<<< HEAD
 
-        if inventory_matches:
-            logging.INFO(inventory_matches.group(0))
-            reply += self.get_inventory(post.author)
-=======
->>>>>>> a525b56b437ef934960a3b2ff8da5048341c104a
         # Matches contains tuples in the format:
         # (@review_bot list, ' network:sub', subreddit, ' keyword', keyword)
         for _, _, sub, _, keyword in list_matches:
