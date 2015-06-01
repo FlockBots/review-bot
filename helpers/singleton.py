a5cae1d3e91f90fd9bad1349fa2235343d0c9898
Stan Janssen <stanjanssen@outlook.com>
2015-06-01 22:28:38 +0200

    Separate get_instance method

--- helpers/singleton.py
+++ helpers/singleton.py
@@ -5,3 +5,9 @@ class Singleton(type):
         if not _class._instance:
             _class._instance = super(Singleton, _class).__call__(*args, **kwargs)
         return _class._instance
+
+    def get_instance(self):
+        if not self._instance:
+            raise AttributeError('Class is not instantiated yet.')
+        return self._instance
+
