7c01217691917e5f0ad6f57cb1ac2222b15de42f
Stan Janssen <stanjanssen@outlook.com>
2015-06-02 16:56:12 +0200

    use cursor_op decorator

--- helpers/database.py
+++ helpers/database.py
@@ -1,6 +1,6 @@
 import sqlite3
 import logging
-
+from helpers.decorators import cursor_op
 
 class Database:
 
@@ -11,19 +11,19 @@ class Database:
         self.logger = logging.getLogger(__name__)
         self._create()
 
-    def _create(self):
+    @cursor_op
+    def _create(self, cursor):
         """ Creates the necessary tables if they do not exists """
         self.logger.debug('Creating table ({})'.format(Database.comment_table))
-        cursor = self.connection.cursor()
         query = (
             'CREATE TABLE IF NOT EXISTS {} '
             '(id TEXT PRIMARY KEY)'
         ).format(Database.comment_table)
         cursor.execute(query)
         self.connection.commit()
-        cursor.close()
 
-    def store_editable(self, editable):
+    @cursor_op
+    def store_editable(self, editable, cursor):
         """ Stores the editable id in the database
 
         To prevent the bot from revisiting comments,
@@ -33,13 +33,12 @@ class Database:
             editable: PRAW comment, submission or message
         """
         self.logger.debug('Storing editable ({})'.format(editable.id))
-        cursor = self.connection.cursor()
         query = 'INSERT INTO {} VALUES (?)'.format(Database.comment_table)
         cursor.execute(query, (editable.id,))
         self.connection.commit()
-        cursor.close()
 
-    def get_editable(self, editable):
+    @cursor_op
+    def get_editable(self, editable, cursor):
         """ Gets the values of editable stored in the database
 
         Args:
@@ -51,9 +50,7 @@ class Database:
                 None
         """
         self.logger.debug('Selecting editable ({})'.format(editable.id))
-        cursor = self.connection.cursor()
         query = 'SELECT * FROM {} WHERE id = ?'.format(Database.comment_table)
         cursor.execute(query, (editable.id,))
         row = cursor.fetchone()
-        cursor.close()
         return row
