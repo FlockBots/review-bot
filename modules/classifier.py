0cc78c78b4d4db9a8adfa2eccf6b92d7801976fc
Stan Janssen <stanjanssen@outlook.com>
2015-06-03 16:22:16 +0200

    added some comments

--- modules/classifier.py
+++ modules/classifier.py
@@ -47,33 +47,61 @@ class Classifier:
         self.classifier = self.train(training_set)
 
     def create_sets(self, database_file):
+        """ Create data sets from the database (assumes table format as defined above).
+
+            Args:
+                database_file: (string) path to the database
+            Returns:
+                A tuple containing the training and test sets.
+                The training set is twice as big as the test set.
+        """
         connection = sqlite3.connect(database_file)
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
 
+        # Get all reviews and all regular comments separately.
         cursor.execute('SELECT comment_text FROM data WHERE class = 1')
         positive = [item[0] for item in cursor.fetchall()]
         cursor.execute('SELECT comment_text FROM data WHERE class = 0')
         negative = [item[0] for item in cursor.fetchall()]
 
+        # calculate sizes
         data_size = len(positive), len(negative)
         train_size = (data_size[0] // 3) * 2, (data_size[1] // 3) * 2
 
+        # shuffle the data
         random.shuffle(positive)
         random.shuffle(negative)
+
+        # create sets
         training_set = DataSet(positive[:train_size[0]], negative[:train_size[1]])
         test_set = DataSet(positive[train_size[0]:], negative[train_size[1]:])
         return training_set, test_set
 
     def train(self, training_set):
+        """ Train the classifier on a training set 
+
+            Args:
+                training_set: (DataSet) data to train on
+            Returns:
+                A trained Linear SVM classifier.
+        """
         train_data = self.vectorizer.fit_transform(training_set)
         train_expected = training_set.classes
 
         classifier = LinearSVC()
         classifier.fit(train_data, train_expected)
-        return classifier, vectorizer
+        return classifier
 
     def test(self, test_set):
+        """ See how well the classifier performs on the given test_set.
+
+            Args:
+                test_set: (DataSet) containing positive and negative samples.
+            Returns:
+                The number of 'True Negatives' (tn), 'False Negatives' (fn), 'True Positives' (tp),
+                 'False Positives' (fp), 'accuracy', 'precision' and 'recall' of the classifier in a dictionary.
+        """
         test_data = self.vectorizer.transform(test_set)
         predictions = self.classifier.predict(test_data)
         results = {'fn': 0, 'fp': 0, 'tn': 0, 'tp': 0}
@@ -90,3 +118,17 @@ class Classifier:
         results['recall'] = results['tp'] / (results['tp'] + results['fn'])
         results['accuracy'] = (results['tp'] + results['tn']) / (results['tp'] + results['tn'] + results['fp'] + results['fn'])
         return results
+
+    def classify(text):
+        """ Classifies a body of text a whisky review or not.
+
+            Args:
+                text: (string) the text to classify
+            Returns:
+                True, if text is a whisky review
+                False, otherwise
+
+        """
+        data = self.vectorizer.transform([text])
+        prediction = self.classifier.predict(data)[0]
+        return prediction == 1
