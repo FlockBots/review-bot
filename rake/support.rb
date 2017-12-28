af0f597de7a464d3cf7a02685ec65748d945a30e
Stan Janssen <mail@janssen.io>
2017-12-28 14:33:11 +0100

    Use Hash#fetch to fail or get default value for env variables

--- rake/support.rb
+++ rake/support.rb
@@ -8,12 +8,12 @@ end
 
 def create_session
   session = Redd.it(
-    user_agent: ENV['RB_USER_AGENT'],
-    client_id:  ENV['RB_CLIENT_ID'],
-    secret:     ENV['RB_SECRET'],
-    username:   ENV['RB_USERNAME'],
-    password:   ENV['RB_PASSWORD'],
-    max_retries: ENV['RB_RETRIES'] || 0
+    user_agent: ENV.fetch('RB_USER_AGENT'),
+    client_id:  ENV.fetch('RB_CLIENT_ID'),
+    secret:     ENV.fetch('RB_SECRET'),
+    username:   ENV.fetch('RB_USERNAME'),
+    password:   ENV.fetch('RB_PASSWORD'),
+    max_retries: ENV.fetch('RB_RETRIES', 0)
   )
 end
 
@@ -21,8 +21,8 @@ def create_logger
   level = ENV['RB_LOGLEVEL'] || 'info'
   Better::Logger.config :logger do |conf|
     conf.color     = false
-    conf.log_to    = ENV['RB_INFOLOG'] || STDOUT
-    conf.error_to  = ENV['RB_ERRORLOG'] || STDERR
+    conf.log_to    = ENV.fetch('RB_INFOLOG', STDOUT)
+    conf.error_to  = ENV.fetch('RB_ERRORLOG', STDERR)
     conf.log_level = level.to_sym
   end
   logger
